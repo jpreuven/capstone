a548cd426fbc3535c31f8110a4dbfe2887cd21fd
Jonathan <jpreuven@gmail.com>
2023-09-06 11:19:16 -0700

    added tenants page

--- server/seed.py
+++ server/seed.py
@@ -34,9 +34,11 @@ if __name__ == '__main__':
 
         bluebell = Property(address = "Bluebell", user_id = "1", purchase_date = datetime(2021, 1, 1))
         oakhurt = Property(address = "Oakhurst", user_id = "1", purchase_date = datetime(2020, 1, 1))
+        cumston = Property(address = "Cumston", user_id = "2", purchase_date = datetime(2023, 1, 1))
 
         db.session.add(bluebell)
         db.session.add(oakhurt)
+        db.session.add(cumston)
 
         
         yoni = Tenant(first_name = "Yoni", last_name = "Reuven", email ="y@y", user_id = "1", active = False, phone_number = "111111111")
@@ -46,6 +48,7 @@ if __name__ == '__main__':
         bob = Tenant(first_name = "Bob", last_name = "smith", email ="b@b", user_id = "1", active = False, phone_number = "5555555555")
         dani = Tenant(first_name = "Dani", last_name = "Azizi", email ="d@d", user_id = "1", active = False, phone_number = "666666666")
         gavriel = Tenant(first_name = "Gabriel", last_name = "Stark", email ="g@g", user_id = "1", active = True, phone_number = "777777777")
+        jonathan = Tenant(first_name = "Jonathan", last_name = "Goldstein", email ="j@j", user_id = "2", active = True, phone_number = "999999999")
 
         db.session.add(yoni)
         db.session.add(debbie)
@@ -54,6 +57,7 @@ if __name__ == '__main__':
         db.session.add(bob)
         db.session.add(dani)
         db.session.add(gavriel)
+        db.session.add(jonathan)
 
         # lease1 = Lease(rent_amount = 500, property_id = "1", tenant_id = "1", start_date=datetime(2021, 1, 1), end_date=datetime(2021, 12, 31))
         # lease2 = Lease(rent_amount = 600, property_id = "1", tenant_id = "2", start_date=datetime(2022, 1, 1), end_date=datetime(2022, 12, 31))
@@ -70,7 +74,7 @@ if __name__ == '__main__':
         # db.session.add(lease6)
         # db.session.add(lease7)
 
-        leases = [Lease(rent_amount = 500, property_id = "1", tenant_id = "1", start_date=datetime(2021, 1, 1), end_date=datetime(2021, 12, 31)), Lease(rent_amount = 600, property_id = "1", tenant_id = "2", start_date=datetime(2022, 1, 1), end_date=datetime(2022, 12, 31)), Lease(rent_amount = 700, property_id = "1", tenant_id = "3", start_date=datetime(2023, 1, 1), end_date=datetime(2023, 12, 31)), Lease(rent_amount = 1500, property_id = "2", tenant_id = "4", start_date=datetime(2020, 1, 1), end_date=datetime(2020, 12, 31)), Lease(rent_amount = 1600, property_id = "2", tenant_id = "5", start_date=datetime(2021, 1, 1), end_date=datetime(2021, 12, 31)),Lease(rent_amount = 1700, property_id = "2", tenant_id = "6", start_date=datetime(2022, 1, 1), end_date=datetime(2022, 12, 31)),Lease(rent_amount = 1700, property_id = "2", tenant_id = "7", start_date=datetime(2023, 1, 1), end_date=datetime(2023, 12, 31))]
+        leases = [Lease(rent_amount = 500, property_id = "1", tenant_id = "1", start_date=datetime(2021, 1, 1), end_date=datetime(2021, 12, 31)), Lease(rent_amount = 600, property_id = "1", tenant_id = "2", start_date=datetime(2022, 1, 1), end_date=datetime(2022, 12, 31)), Lease(rent_amount = 700, property_id = "1", tenant_id = "3", start_date=datetime(2023, 1, 1), end_date=datetime(2023, 12, 31)), Lease(rent_amount = 1500, property_id = "2", tenant_id = "4", start_date=datetime(2020, 1, 1), end_date=datetime(2020, 12, 31)), Lease(rent_amount = 1600, property_id = "2", tenant_id = "5", start_date=datetime(2021, 1, 1), end_date=datetime(2021, 12, 31)),Lease(rent_amount = 1700, property_id = "2", tenant_id = "6", start_date=datetime(2022, 1, 1), end_date=datetime(2022, 12, 31)),Lease(rent_amount = 1700, property_id = "2", tenant_id = "7", start_date=datetime(2023, 1, 1), end_date=datetime(2023, 12, 31)),Lease(rent_amount = 1000, property_id = "3", tenant_id = "8", start_date=datetime(2023, 1, 1), end_date=datetime(2023, 12, 31))]
 
         for lease in leases:
             db.session.add(lease)
@@ -100,7 +104,7 @@ if __name__ == '__main__':
                 month += 1
         test_bill = Bill(date=datetime(2024, 1, 1), lease_id = 3)
         db.session.add(test_bill)
-        test_charge = Charge(type_of_charge="rent", amount = 700, bill_id = 85)
+        test_charge = Charge(type_of_charge="rent", amount = 700, bill_id = 97)
         db.session.add(test_charge)
 
         db.session.commit()
