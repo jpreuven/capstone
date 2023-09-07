e6112cf6c25ca797ff68e0ee02d106c1840c7f65
Jonathan <jpreuven@gmail.com>
2023-09-06 21:06:26 -0700

    started Charge Form

--- client/src/components/Main/BillForm.js
+++ client/src/components/Main/BillForm.js
@@ -1,35 +1,75 @@
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 import { Field, Formik } from "formik";
 import * as yup from "yup";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 export const BillForm = () => {
+  const [startDate, setStartDate] = useState(new Date());
   const formSchema = yup.object().shape({
-    name: yup.string(),
+    date: yup.date().required("Date is required"),
   });
+  const tenant = useSelector((state) => state.tenant.value);
 
   function handleSubmitBill(value) {
-    console.log(value);
+    const new_bill = {
+      date: value.date,
+      lease_id: tenant.leases[0].id,
+    };
+    fetch("/bills", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(new_bill),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((bill) => {
+          console.log(bill);
+          //   const updatedCollections = [...collections, collection];
+          //   setCollections(updatedCollections);
+          //   history.push("/collections");
+        });
+      } else {
+        res.json().then((error) => {
+          console.log(error);
+        });
+      }
+    });
   }
 
   return (
-    // <div>
     <Formik
-      initialValues={{ data: "" }}
+      initialValues={{ date: `${new Date().toLocaleDateString("en-CA")}` }}
       validationSchema={formSchema}
-      onSubmit={handleSubmitBill} // Wrap the console.log in a function
+      onSubmit={handleSubmitBill}
     >
       {(props) => (
         <form onSubmit={props.handleSubmit}>
           <FormControl isInvalid={!!props.errors.name && props.touched.name}>
             <FormLabel htmlFor="name">Bill Form:</FormLabel>
-            <Field
-              as={Input}
-              id="name"
-              name="name"
-              onChange={props.handleChange} // Use props.handleChange to handle the change
+            <DatePicker
+              selected={startDate}
+              minDate={new Date()}
+              onChange={(date) => {
+                setStartDate(date);
+                const formattedDate = date.toLocaleDateString("en-CA");
+                props.setFieldValue("date", formattedDate);
+              }}
+              dateFormat="MM/dd/yyyy"
+              //   renderDayContents={renderDayContents}
             />
+
+            <FormErrorMessage>{props.errors.name}</FormErrorMessage>
             <Button type="submit">Submit</Button>
           </FormControl>
         </form>
--- client/src/components/Main/ChargeForm.js
+++ client/src/components/Main/ChargeForm.js
@@ -1,4 +1,12 @@
-import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  HStack,
+  Stack,
+  Flex,
+} from "@chakra-ui/react";
 import { Field, Formik } from "formik";
 import * as yup from "yup";
 
@@ -6,74 +14,45 @@ import React from "react";
 
 export const ChargeForm = () => {
   const formSchema = yup.object().shape({
-    name: yup.string(),
+    type_of_charge: yup.string().required("Please enter a type of charge"),
+    amount: yup.number().required("Please enter charge amount"),
   });
 
   return (
-    <div>
-      hello?
-      <Formik
-        initialValues={{ name: "" }}
-        validationSchema={formSchema}
-        onSubmit={() => console.log("hi")} // Wrap the console.log in a function
-      >
-        {(props) => (
-          <form>
+    <Formik
+      initialValues={{ type_of_charge: "", amount: "" }}
+      validationSchema={formSchema}
+      onSubmit={(value) => console.log(value)} // Wrap the console.log in a function
+    >
+      {(props) => (
+        <form onSubmit={props.handleSubmit}>
+          <Stack spacing={2}>
             <FormControl isInvalid={!!props.errors.name && props.touched.name}>
-              <FormLabel htmlFor="name">Charge Form:</FormLabel>
+              <FormLabel htmlFor="type_of_charge">Type of Charge:</FormLabel>
               <Field
                 as={Input}
-                id="name"
-                name="name"
-                onChange={props.handleChange} // Use props.handleChange to handle the change
+                id="type_of_charge"
+                name="type_of_charge"
+                onChange={props.handleChange}
               />
-              <Button type="submit">Submit</Button>
             </FormControl>
-          </form>
-        )}
-      </Formik>
-    </div>
-    // <Formik
-    //   initialValues={{ name: "", image: "" }}
-    //   validationSchema={formSchema}
-    //   onSubmit={handlePostCollection}
-    // >
-    //   {(props) => (
-    //     <form onSubmit={props.handleSubmit}>
-    //       <FormControl isInvalid={!!props.errors.name && props.touched.name}>
-    //         <FormLabel htmlFor="name">Collection Name:</FormLabel>
-    //         <Field
-    //           as={Input}
-    //           id="name"
-    //           name="name"
-    //           onChange={Formik.handleChange}
-    //         />
-    //         <FormErrorMessage>{props.errors.name}</FormErrorMessage>
-    //       </FormControl>
-    //       <FormControl
-    //         isInvalid={!!props.errors.avatar && props.touched.avatar}
-    //       >
-    //         <FormLabel mt="5" htmlFor="avatar">
-    //           Choose an Icon:
-    //         </FormLabel>
-    //         <Field
-    //           as={RadioGroup}
-    //           id="avatar"
-    //           name="avatar"
-    //           py={2}
-    //           display="flex"
-    //           gridColumnGap={2}
-    //         >
-    //           {AVATARS.map(({ image }) => {
-    //             return <ImageRadio key={image} image={image} value={image} />;
-    //           })}
-    //         </Field>
-    //         <FormErrorMessage>{props.errors.avatar}</FormErrorMessage>
-    //       </FormControl>
-    //       <Button type="submit">Submit</Button>
-    //     </form>
-    //   )}
-    // </Formik>
+            <FormControl
+              isInvalid={!!props.errors.amount && props.touched.amount}
+            >
+              <FormLabel htmlFor="amount">Amount:</FormLabel>
+              <Field
+                type="number"
+                as={Input}
+                id="amount"
+                name="amount"
+                onChange={props.handleChange}
+              />
+            </FormControl>
+            <Button type="submit">Submit</Button>
+          </Stack>
+        </form>
+      )}
+    </Formik>
   );
 };
 
